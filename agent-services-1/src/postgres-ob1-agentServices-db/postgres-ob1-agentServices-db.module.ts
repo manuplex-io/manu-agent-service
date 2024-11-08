ba05ddccd9d2d417325037235a7ed870cfe98222
Abhishek <abhishek@manuplex.io>
2024-11-08 12:41:15 +0530

    changes

--- agent-services-1/src/postgres-ob1-agentServices-db/postgres-ob1-agentServices-db.module.ts
+++ agent-services-1/src/postgres-ob1-agentServices-db/postgres-ob1-agentServices-db.module.ts
@@ -7,7 +7,6 @@ import { OB1AgentTools } from '../entities/ob1-agent-tools.entity';
 import { OB1ToolCategory } from '../entities/ob1-agent-toolCategory.entity';
 import { OB1ToolExecutionLog } from 'src/entities/ob1-agent-toolExecutionLog.entity';
 
-
 @Module({
   imports: [
     ConfigModule.forRoot({
@@ -17,23 +16,25 @@ import { OB1ToolExecutionLog } from 'src/entities/ob1-agent-toolExecutionLog.ent
       type: 'postgres',
       host: process.env.OB1_DB_HOST,
       port: +process.env.OB1_DB_PORT,
-      username: process.env.ENV === 'dev' ? process.env.OB1_DB_USERNAME_AGENTSERVICE_DEV : process.env.OB1_DB_USERNAME_AGENTSERVICE,
-      password: process.env.ENV === 'dev' ? process.env.OB1_DB_PASSWORD_AGENTSERVICE_DEV : process.env.OB1_DB_PASSWORD_AGENTSERVICE,
+      username:
+        process.env.ENV === 'dev'
+          ? process.env.OB1_DB_USERNAME_AGENTSERVICE_DEV
+          : process.env.OB1_DB_USERNAME_AGENTSERVICE,
+      password:
+        process.env.ENV === 'dev'
+          ? process.env.OB1_DB_PASSWORD_AGENTSERVICE_DEV
+          : process.env.OB1_DB_PASSWORD_AGENTSERVICE,
       database: process.env.OB1_DB_DATABASE_AGENTSERVICE,
-      entities: [
-        OB1AgentTools,
-        OB1ToolCategory,
-        OB1ToolExecutionLog
-      ], // Add the entities relevant to this DB
+      entities: [OB1AgentTools, OB1ToolCategory, OB1ToolExecutionLog], // Add the entities relevant to this DB
       // synchronize: true,
-      synchronize: process.env.ENV === 'dev',  // Only synchronize in the 'dev' environment
+      synchronize: process.env.ENV === 'dev', // Only synchronize in the 'dev' environment
     }),
     TypeOrmModule.forFeature([
       OB1AgentTools,
       OB1ToolCategory,
-      OB1ToolExecutionLog
+      OB1ToolExecutionLog,
     ]), // Register the entities for repositories in your services
   ],
   exports: [TypeOrmModule],
 })
-export class PostgresOb1AgentServicesDbModule { }
+export class PostgresOb1AgentServicesDbModule {}
